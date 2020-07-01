5da3cef956b56321ceffb9285bb2c48810d6be02
Andres De Castro <af.decastro879@gmail.com>
2020-07-01 18:19:28 -0500

    Publish to NPM when merging to master

--- build/ci/tag_code.sh
+++ build/ci/tag_code.sh
@@ -11,6 +11,14 @@ f_info_log "Calculating tag ${TRAVIS_BRANCH} branch..."
 tag=v$(docker run --rm -v "$(pwd):/repo" gittools/gitversion:5.2.4-linux-ubuntu-18.04-netcoreapp3.1 /repo -output json -showvariable SemVer)
 f_info_log "The tag for current source code is: ${tag}"
 
+# Bump up npm version if is a master release
+if [ "${TRAVIS_BRANCH}" = "master" ]; then
+  npm version "$tag" --no-git-tag-version
+  git add package.json
+  git commit -m "[skip travis] Bump up NPM version to ${tag}"
+  git push -q "https://${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG}" "$TRAVIS_BRANCH"
+fi
+
 # Create tag in github
 f_info_log "Pushing tag to github..."
 curl -s -X POST https://api.github.com/repos/"$TRAVIS_REPO_SLUG"/git/refs -H "Authorization: token $GITHUB_TOKEN" \
