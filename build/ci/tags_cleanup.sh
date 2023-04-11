4020609de9df1acbf26f78bdb8b9474b16dfeab9
Helgard <48946187+meyerhp@users.noreply.github.com>
2023-04-11 16:08:58 +0200

    Merge develop (#55)
    * fix(PRS-2317): fixed multiline table cells
    
    * fix(PRS-2317): prefix links with baseurl shortcode
    
    * Bump to version 2
    
    * Include amd64 in goreleaser build
    
    * fix(PRS-2563): escape description, generate sub schemas, fix urls for nested schemas (#50)
    
    * fix/PRSDM-2831-filepath-sort
    
    * fix(PRS-3815): Configure brew release for arm64 support
    
    * Update Github key
    
    * Update Github token and deprecated config
    
    ---------
    
    Co-authored-by: Damian Hunter <damianhunter@gmail.com>

--- build/ci/tags_cleanup.sh
+++ build/ci/tags_cleanup.sh
@@ -8,6 +8,6 @@ f_info_log "Cleaning old RFV tags"
 # Delete old alpha tags on release
 numTags=$(git tag -l "*-rfv.*" | wc -l | xargs)
 if [ $numTags -gt 0 ]; then
-  git push -q -d "https://${GITHUB_KEY}@github.com/${TRAVIS_REPO_SLUG}" $(git tag -l "*-rfv.*")
+  git push -q -d "https://${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG}" $(git tag -l "*-rfv.*")
 fi
 f_info_log "RFV tags cleaned successfully"
